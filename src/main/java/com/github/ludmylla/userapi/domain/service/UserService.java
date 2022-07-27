173c4676a43bfb37636f505f3edf6508453f6027
Ludmylla <ludmay4@gmail.com>
2022-07-27 09:28:01 -0300

    Criando metodo create para a classe user resource

--- src/main/java/com/github/ludmylla/userapi/domain/service/UserService.java
+++ src/main/java/com/github/ludmylla/userapi/domain/service/UserService.java
@@ -1,5 +1,6 @@
 package com.github.ludmylla.userapi.domain.service;
 
+import com.github.ludmylla.userapi.domain.dto.UserDTO;
 import com.github.ludmylla.userapi.domain.model.User;
 
 import java.util.List;
@@ -9,5 +10,8 @@ public interface UserService {
     User findById(Long id);
 
     List<User> findAll();
+
+    User create (UserDTO dto);
+
 
 }
--- src/main/java/com/github/ludmylla/userapi/domain/service/impl/UserServiceImpl.java
+++ src/main/java/com/github/ludmylla/userapi/domain/service/impl/UserServiceImpl.java
@@ -1,19 +1,26 @@
 package com.github.ludmylla.userapi.domain.service.impl;
 
+import com.github.ludmylla.userapi.domain.dto.UserDTO;
 import com.github.ludmylla.userapi.domain.model.User;
 import com.github.ludmylla.userapi.domain.repository.UserRepository;
 import com.github.ludmylla.userapi.domain.service.UserService;
+import com.github.ludmylla.userapi.domain.service.exceptions.DataIntegrityViolationException;
 import com.github.ludmylla.userapi.domain.service.exceptions.ObjectNotFoundException;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private ModelMapper mapper;
 
     @Override
     public User findById(Long id) {
@@ -24,5 +31,19 @@ public class UserServiceImpl implements UserService {
     @Override
     public List<User> findAll() {
         return userRepository.findAll();
+    }
+
+    @Override
+    public User create(UserDTO dto) {
+        findByEmailUsed(dto);
+        return userRepository.save(mapper.map(dto, User.class));
+    }
+
+    private void findByEmailUsed(UserDTO dto){
+      Optional<User> user = userRepository.findByEmail(dto.getEmail());
+
+      if(user.isPresent() && !user.get().getId().equals(dto.getId())){
+        throw new DataIntegrityViolationException("Email in use");
+      }
     }
 }
