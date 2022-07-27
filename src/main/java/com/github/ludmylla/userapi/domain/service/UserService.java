4d91c94766d3777ceca61d22edab7c4a34a200e2
Ludmylla <ludmay4@gmail.com>
2022-07-27 09:14:36 -0300

    Criando metodo findAll para classe user service

--- src/main/java/com/github/ludmylla/userapi/domain/service/UserService.java
+++ src/main/java/com/github/ludmylla/userapi/domain/service/UserService.java
@@ -2,8 +2,12 @@ package com.github.ludmylla.userapi.domain.service;
 
 import com.github.ludmylla.userapi.domain.model.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Long id);
+
+    List<User> findAll();
 
 }
--- src/main/java/com/github/ludmylla/userapi/domain/service/impl/UserServiceImpl.java
+++ src/main/java/com/github/ludmylla/userapi/domain/service/impl/UserServiceImpl.java
@@ -7,6 +7,8 @@ import com.github.ludmylla.userapi.domain.service.exceptions.ObjectNotFoundExcep
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserServiceImpl implements UserService {
 
@@ -17,5 +19,10 @@ public class UserServiceImpl implements UserService {
     public User findById(Long id) {
         return userRepository.findById(id)
                 .orElseThrow(() -> new ObjectNotFoundException("Object not found."));
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
     }
 }
