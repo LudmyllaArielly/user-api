13a29ff2a5597d6620b132e78d3aa7ef7bad28e2
Ludmylla <ludmay4@gmail.com>
2022-07-27 10:03:06 -0300

    Teste para classe user service metodo findById com sucesso

---
+++ src/test/java/com/github/ludmylla/userapi/domain/service/impl/UserServiceImplTest.java
@@ -0,0 +1,89 @@
+package com.github.ludmylla.userapi.domain.service.impl;
+
+import com.github.ludmylla.userapi.domain.dto.UserDTO;
+import com.github.ludmylla.userapi.domain.model.User;
+import com.github.ludmylla.userapi.domain.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+
+import static org.mockito.Mockito.anyLong;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Optional;
+
+class UserServiceImplTest {
+
+    public static final Long ID = 1L;
+    public static final String NAME = "Maria";
+    public static final String EMAIL = "maria@xyz.com";
+    public static final String PASSWORD = "123";
+
+
+    @Mock
+    UserRepository userRepository;
+
+    @Mock
+    ModelMapper mapper;
+
+    @InjectMocks
+    UserServiceImpl userServiceImpl;
+
+    private User user;
+    private UserDTO dto;
+    private Optional<User> userOptional;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        prepareData();
+    }
+
+    @Test
+    void shouldReturnSuccess_WhenToConsultUser() {
+        Mockito.when(userRepository.findById(anyLong())).thenReturn(userOptional);
+
+        User response = userServiceImpl.findById(ID);
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+
+    }
+
+    @Test
+    void testFindAll() {
+
+    }
+
+    @Test
+    void testCreate() {
+
+    }
+
+    @Test
+    void testUpdate() {
+
+    }
+
+    @Test
+    void testDelete() {
+    }
+
+    private void prepareData() {
+        user = new User(ID, NAME, EMAIL, PASSWORD);
+        dto = new UserDTO(ID, NAME, EMAIL, PASSWORD);
+        userOptional = Optional.of(new User(ID, NAME, EMAIL, PASSWORD));
+    }
+}
+
+
