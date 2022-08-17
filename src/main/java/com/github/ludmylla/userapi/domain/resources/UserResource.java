a3dc7a617dfab62e02688be7b60c4b1ebe8e3e0e
Ludmylla <ludmay4@gmail.com>
2022-08-17 15:59:27 -0300

    criando método para buscar usuário por email

--- src/main/java/com/github/ludmylla/userapi/domain/resources/UserResource.java
+++ src/main/java/com/github/ludmylla/userapi/domain/resources/UserResource.java
@@ -1,13 +1,11 @@
 package com.github.ludmylla.userapi.domain.resources;
 
 import com.github.ludmylla.userapi.domain.model.User;
+import com.github.ludmylla.userapi.domain.repository.UserRepository;
 import com.github.ludmylla.userapi.domain.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -16,9 +14,20 @@ public class UserResource {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/{id}")
     public ResponseEntity<User> findByUser(@PathVariable Long id){
         User user = userService.findById(id);
         return ResponseEntity.ok(user);
     }
+
+    @GetMapping("/search")
+    public ResponseEntity<User> findByEmail(@RequestParam String email){
+        User user = userRepository.findByEmail(email);
+        return ResponseEntity.ok(user);
+    }
+
+
 }
