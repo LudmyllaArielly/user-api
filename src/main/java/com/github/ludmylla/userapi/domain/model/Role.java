9dbdcbcf684c0b852f45ba8c66510b020dee37c6
Ludmylla <ludmay4@gmail.com>
2022-08-17 14:46:10 -0300

    criando classe role

---
+++ src/main/java/com/github/ludmylla/userapi/domain/model/Role.java
@@ -0,0 +1,30 @@
+package com.github.ludmylla.userapi.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "tb_role")
+public class Role implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String roleName;
+
+
+
+
+}
