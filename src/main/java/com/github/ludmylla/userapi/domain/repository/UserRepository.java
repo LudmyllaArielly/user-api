d46be7b18c56cb35b78f04102f444068c454ee17
Ludmylla <ludmay4@gmail.com>
2022-08-17 15:59:00 -0300

    editando método find by email

--- src/main/java/com/github/ludmylla/userapi/domain/repository/UserRepository.java
+++ src/main/java/com/github/ludmylla/userapi/domain/repository/UserRepository.java
@@ -4,10 +4,8 @@ import com.github.ludmylla.userapi.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByEmail(String email);
+    User findByEmail(String email);
 }
