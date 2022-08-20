3531c49ef4a2bee38ac2b2eea5005a81f1bc0991
Ludmylla <ludmay4@gmail.com>
2022-08-20 13:05:29 -0300

    criando classe utilitaria

---
+++ src/main/java/com/github/ludmylla/userapi/util/Messages.java
@@ -0,0 +1,11 @@
+package com.github.ludmylla.userapi.util;
+
+public class Messages {
+
+    public static final String MSG_ERROR_GENERIC_USER_FINAL = "An unexpected internal system error has occurred. " +
+            "Please try again and if the problem persists, please contact us. " +
+            "with your system administrator.";
+
+    public static final String MSG_EMAIL_IN_USE= "Email is in use";
+
+}
