ef8bb13d618b014fd3637975530be5a57c1771b5
Ludmylla <ludmay4@gmail.com>
2022-08-20 13:06:50 -0300

    criando método de busca email optional

--- src/main/java/com/github/ludmylla/userapi/domain/repository/UserRepository.java
+++ src/main/java/com/github/ludmylla/userapi/domain/repository/UserRepository.java
@@ -2,11 +2,18 @@ package com.github.ludmylla.userapi.domain.repository;
 
 import com.github.ludmylla.userapi.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByEmail(String email);
+
+    @Query("from User u where u.email = :email")
+    Optional<User> findByEmailOptional(@Param("email") String email);
 
 }
