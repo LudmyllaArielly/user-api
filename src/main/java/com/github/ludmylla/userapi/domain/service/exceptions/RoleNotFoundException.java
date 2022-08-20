a881e0d1ef291e92e6b1c1a9904a0dd3281b5a55
Ludmylla <ludmay4@gmail.com>
2022-08-20 11:42:03 -0300

    extends de entity not found exception e adicionando response status Not found

--- src/main/java/com/github/ludmylla/userapi/domain/service/exceptions/RoleNotFoundException.java
+++ src/main/java/com/github/ludmylla/userapi/domain/service/exceptions/RoleNotFoundException.java
@@ -1,13 +1,17 @@
 package com.github.ludmylla.userapi.domain.service.exceptions;
 
-public class RoleNotFoundException extends RuntimeException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class RoleNotFoundException extends EntityNotFoundException {
     private static final long serialVersionUID = -1L;
 
     public RoleNotFoundException(String message) {
         super(message);
     }
 
-    public RoleNotFoundException(String message, Throwable cause) {
-        super(message, cause);
+    public RoleNotFoundException(Long id) {
+        this(String.format("There is no such code in Role %d" ,id));
     }
 }
--- src/main/java/com/github/ludmylla/userapi/domain/service/exceptions/UserNotFoundException.java
+++ src/main/java/com/github/ludmylla/userapi/domain/service/exceptions/UserNotFoundException.java
@@ -1,13 +1,19 @@
 package com.github.ludmylla.userapi.domain.service.exceptions;
 
-public class UserNotFoundException extends RuntimeException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends EntityNotFoundException {
+
     private static final long serialVersionUID = -1L;
 
     public UserNotFoundException(String message) {
         super(message);
     }
 
-    public UserNotFoundException(String message, Throwable cause) {
-        super(message, cause);
+    public UserNotFoundException(Long id) {
+        this(String.format("There is no such code in User %d" ,id));
     }
+
 }
