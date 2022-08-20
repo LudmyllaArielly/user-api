37e2cf3c42cf9d6c96c039c4891d26595cb2a5d1
Ludmylla <ludmay4@gmail.com>
2022-08-20 10:05:01 -0300

    criando dtos para login e token

---
+++ src/main/java/com/github/ludmylla/userapi/domain/dto/LoginDTO.java
@@ -0,0 +1,14 @@
+package com.github.ludmylla.userapi.domain.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginDTO {
+
+    private String email;
+    private String password;
+
+
+}
