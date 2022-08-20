368c15f57522da04ce7777e77e3dd5c0dda217ae
Ludmylla <ludmay4@gmail.com>
2022-08-20 11:40:56 -0300

    colocando response status conflict

--- src/main/java/com/github/ludmylla/userapi/domain/service/exceptions/DataIntegrityViolationException.java
+++ src/main/java/com/github/ludmylla/userapi/domain/service/exceptions/DataIntegrityViolationException.java
@@ -1,5 +1,9 @@
 package com.github.ludmylla.userapi.domain.service.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
 public class DataIntegrityViolationException extends RuntimeException {
     private static final long serialVersionUID = -1L;
 
