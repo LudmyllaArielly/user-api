a458e9cbd9d3346dc47ce22dba0659c9fdb376d7
Ludmylla <ludmay4@gmail.com>
2022-08-24 12:47:56 -0300

    adicionando tratamento de exceção para credenciais inválidas e validação de campos.

--- src/main/java/com/github/ludmylla/userapi/api/exceptionHandler/ApiExceptionHandler.java
+++ src/main/java/com/github/ludmylla/userapi/api/exceptionHandler/ApiExceptionHandler.java
@@ -3,11 +3,18 @@ package com.github.ludmylla.userapi.api.exceptionHandler;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.github.ludmylla.userapi.domain.service.exceptions.BusinessException;
 import com.github.ludmylla.userapi.domain.service.exceptions.EntityNotFoundException;
+import com.github.ludmylla.userapi.domain.service.exceptions.UserBadCredentialsException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -22,6 +29,75 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Autowired
     private MessageSource messageSource;
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ProblemType problemType = ProblemType.INVALID_DATA;
+        String details = "One or more fields are invalid. Please fill in correctly and try again.";
+
+        BindingResult bindingResult = ex.getBindingResult();
+
+        List<Problem.Object> problemFields = bindingResult.getAllErrors().stream()
+                .map(objectError -> {
+                    String message = messageSource.getMessage(objectError, LocaleContextHolder.getLocale());
+                    String name = objectError.getObjectName();
+
+                    if(objectError instanceof FieldError){
+                        name = ((FieldError) objectError).getField();
+                    }
+
+                    return Problem.Object.builder()
+                            .name(name)
+                            .userMessage(message).build();
+
+                })
+                .collect(Collectors.toList());
+
+        Problem problem = createProblemBuilder(status, problemType, details)
+                .userMessage(details)
+                .objects(problemFields)
+                .build();
+
+        return handleExceptionInternal(ex, problem, new HttpHeaders(),status, request);
+    }
+
+    private ResponseEntity<Object> handleValidationInternal(Exception ex, BindingResult bindingResult, HttpHeaders headers, HttpStatus status, WebRequest request){
+        ProblemType problemType = ProblemType.INVALID_DATA;
+        String details = "One or more fields are invalid. Please fill in correctly and try again.";
+
+        List<Problem.Object> problemObjects = bindingResult.getAllErrors().stream()
+                .map(objectError -> {
+                    String message = messageSource.getMessage(objectError, LocaleContextHolder.getLocale());
+
+                    String name = objectError.getObjectName();
+
+                    if(objectError instanceof FieldError){
+                        name = ((FieldError) objectError).getField();
+                    }
+
+                    return Problem.Object.builder()
+                            .name(name)
+                            .userMessage(message).build();
+                })
+                .collect(Collectors.toList());
+
+        Problem problem = createProblemBuilder(status, problemType, details)
+                .userMessage(details)
+                .objects(problemObjects)
+                .build();
+
+        return handleExceptionInternal(ex, problem, new HttpHeaders(),status, request);
+    }
+
+    @ExceptionHandler(UserBadCredentialsException.class)
+    public ResponseEntity<?> handleBadCredentialsException(UserBadCredentialsException ex, WebRequest request){
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        ProblemType problemType = ProblemType.BAD_CREDENTIALS;
+        String detail = ex.getMessage();
+
+        Problem problem = createProblemBuilder(status, problemType, detail).build();
+        return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
+    }
 
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<?> handleBusinessException(BusinessException ex, WebRequest request){
@@ -51,12 +127,14 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
                     .timestamp(OffsetDateTime.now())
                     .status(status.value())
                     .title(status.getReasonPhrase())
+                    .userMessage("message padrao")
                     .build();
         }else if (body instanceof String) {
             body = Problem.builder()
                     .timestamp(OffsetDateTime.now())
                     .status(status.value())
                     .title((String) body)
+                    .userMessage("message padrao")
                     .build();
         }
         return super.handleExceptionInternal(ex, body, headers, status, request);
