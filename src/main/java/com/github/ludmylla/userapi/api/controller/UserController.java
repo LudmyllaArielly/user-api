27cbc5f8506ac459ccc90d7fe6c2cd98bcc9dff9
Ludmylla <ludmay4@gmail.com>
2022-08-24 15:16:42 -0300

    removendo import não usado

--- src/main/java/com/github/ludmylla/userapi/api/controller/UserController.java
+++ src/main/java/com/github/ludmylla/userapi/api/controller/UserController.java
@@ -13,6 +13,7 @@ import com.github.ludmylla.userapi.security.TokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
