3a7ca75c22c1322cfaea623164080241119187e1
Ludmylla <ludmay4@gmail.com>
2022-08-24 12:48:46 -0300

    criando enum em problem type

--- src/main/java/com/github/ludmylla/userapi/api/exceptionHandler/ProblemType.java
+++ src/main/java/com/github/ludmylla/userapi/api/exceptionHandler/ProblemType.java
@@ -7,7 +7,9 @@ public enum ProblemType {
 
     RESOURCE_NOT_FOUND("/resource-not-found", "Resource not found"),
     ENTITY_IN_USE("/entity-in-use", "Entity in use"),
-    ERROR_BUSINESS("/error-business", "Error business");
+    ERROR_BUSINESS("/error-business", "Error business"),
+    BAD_CREDENTIALS("/bad-credentials", "Bad credentials"),
+    INVALID_DATA("/invalid-data","Invalid data.");
 
     private String title;
     private String uri;
