711b3e9cc183c0ca2b0a84cc5ab95777a30acf09
Ludmylla <ludmay4@gmail.com>
2022-08-24 12:51:47 -0300

    adicionando anotações de validação no login dto

--- src/main/java/com/github/ludmylla/userapi/domain/dto/LoginDTO.java
+++ src/main/java/com/github/ludmylla/userapi/domain/dto/LoginDTO.java
@@ -3,12 +3,16 @@ package com.github.ludmylla.userapi.domain.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 public class LoginDTO {
 
+    @NotBlank
     private String email;
-    private String password;
 
+    @NotBlank
+    private String password;
 
 }
