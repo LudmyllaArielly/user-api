40b220cdb8dd18f63db1d1a3bf5ba5a728e37a30
Ludmylla <ludmay4@gmail.com>
2022-08-24 13:33:18 -0300

    removendo anotações de validação da classe usuário

--- src/main/java/com/github/ludmylla/userapi/domain/model/User.java
+++ src/main/java/com/github/ludmylla/userapi/domain/model/User.java
@@ -1,13 +1,11 @@
 package com.github.ludmylla.userapi.domain.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -23,17 +21,12 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @NotBlank
     private String name;
 
-    @NotBlank
     @Column(unique = true)
+
     private String email;
-
-    @NotBlank
     private String password;
-
 
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "tb_user_role",
