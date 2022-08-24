98c93dbc49a4a65c79b9cf4959ff426da056d39d
Ludmylla <ludmay4@gmail.com>
2022-08-24 15:15:46 -0300

    criando novos enums

--- src/main/java/com/github/ludmylla/userapi/api/exceptionHandler/ProblemType.java
+++ src/main/java/com/github/ludmylla/userapi/api/exceptionHandler/ProblemType.java
@@ -9,10 +9,13 @@ public enum ProblemType {
     ENTITY_IN_USE("/entity-in-use", "Entity in use"),
     ERROR_BUSINESS("/error-business", "Error business"),
     BAD_CREDENTIALS("/bad-credentials", "Bad credentials"),
-    INVALID_DATA("/invalid-data","Invalid data.");
+    INVALID_DATA("/invalid-data","Invalid data."),
+    SYSTEM_ERROR("/system-error","System error."),
+    INVALID_PARAMETER("/invalid-parameter", "Invalid parameter."),
+    MESSAGE_INCOMPRESSIBLE("/message-incompressible.", "Message incompressible");
 
-    private String title;
     private String uri;
+    private String title;
 
     ProblemType(String path, String title){
         this.uri = "https://ludmyllafarias.com" + path;
