e5cdca82e59166a363fef7b968017cbe6b46d1d5
Ludmylla <ludmay4@gmail.com>
2022-09-06 16:43:55 -0300

    adicionando anotação lombok em logindto

--- src/main/java/com/github/ludmylla/userapi/domain/dto/LoginDTO.java
+++ src/main/java/com/github/ludmylla/userapi/domain/dto/LoginDTO.java
@@ -1,10 +1,13 @@
 package com.github.ludmylla.userapi.domain.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
-
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 public class LoginDTO {
