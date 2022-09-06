03693dd94aad3912d9909723e41991d1b1b494f7
Ludmylla <ludmay4@gmail.com>
2022-09-06 12:23:26 -0300

    anotando com lombok

--- src/main/java/com/github/ludmylla/userapi/domain/dto/input/RoleIdInput.java
+++ src/main/java/com/github/ludmylla/userapi/domain/dto/input/RoleIdInput.java
@@ -1,14 +1,19 @@
 package com.github.ludmylla.userapi.domain.dto.input;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 public class RoleIdInput {
 
     @NotNull
     private Long id;
+
 }
